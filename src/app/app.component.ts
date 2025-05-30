cab927771fd3a6f0928df29d1629cb8b4e6d2964
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-05-30 13:52:53 +0300

    refactor: update loading state and fetch user data from new API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,21 @@
 import { Component, effect, OnInit, resource, signal } from '@angular/core';
-
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   constructor() {
-    effect(() => {
-      if (this.userResource.value())
-        this.user.set(this.userResource.value().joke);
-    });
   }
   userResource = resource({
     loader: async () => {
-      const response = await fetch('https://icanhazdadjok.com/', {
-        headers: {
-          Accept: 'application/json',
-        },
-      });
-      return response.json();
-    },
+      await delay(2000);
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+      const data = await response.json();
+      return data.title;
+    }
   });
-  user = signal<string | null>(null);
-  ngOnInit() {}
 }
